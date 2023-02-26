1fa72d7543fb3a3e46b6be73cca12c646cf75b4b
sidarhalman <sidarhalman93@gmail.com>
2023-02-26 16:51:24 +0100

    du hast falsch importiert

--- task-04/src/components/Form.jsx
+++ task-04/src/components/Form.jsx
@@ -1,16 +1,30 @@
-import {useContext} from 'react'
-import {UserContext} from '../../context/UserContext'
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Form = () => {
-  const user = useContext(UserContext)  
+  const user = useContext(UserContext);
   return (
-    <div className='Form'>
+    <div className="Form">
       <form>
-        <label>Name: <input className='input-item' type="text" onChange={(e) => user.setName(e.target.value)} /></label>
-        <label>Location: <input className='input-item' type="text" onChange={(e) => user.setLocation(e.target.value)} /></label>
+        <label>
+          Name:{" "}
+          <input
+            className="input-item"
+            type="text"
+            onChange={(e) => user.setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Location:{" "}
+          <input
+            className="input-item"
+            type="text"
+            onChange={(e) => user.setLocation(e.target.value)}
+          />
+        </label>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
